fd08ab7c1e1ecb039cc36650a1abbb45c6a6d4bb
jianli (Jian Li) <lijian.pku@gmail.com>
2011-08-03 09:10:23 +0000

    v1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rweibo@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RsegWord/R/RsegWord-package.R
@@ -0,0 +1,27 @@
+
+##' Use the imdict-chinese-analyzer (see \url{http://ictclas.org/news_ictclas_code.html}) to do words segmentation in R.
+##' 
+##' \tabular{ll}{ 
+##' Package: \tab Rweibo\cr 
+##' Type: \tab Package\cr 
+##' Version: \tab 0.0-1\cr
+##' Date: \tab 2011-08-03\cr 
+##' License: \tab GPL\cr 
+##' LazyLoad: \tab yes\cr 
+##' } 
+##' 
+##' 
+##' @title A package to segment Chinese sentence
+##' @name RsegWord-package
+##' @aliases RsegWord-package RsegWord
+##' @docType package
+##' @author Jian li
+##' 
+##' Maintainer: Jian Li <rweibo@@sina.com>
+##' @references \url{http://ictclas.org/}
+##' @keywords package
+##' 
+NULL
+
+
+
